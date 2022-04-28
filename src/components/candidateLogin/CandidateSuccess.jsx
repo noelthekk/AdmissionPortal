28ea9f215e56187500597d55bf684c238ff917cc
rj777pain <rejathtm@gmail>
2022-04-28 11:19:02 +0530

    added alignment to candidate success

--- src/components/candidateLogin/CandidateSuccess.jsx
+++ src/components/candidateLogin/CandidateSuccess.jsx
@@ -3,9 +3,9 @@ import React, { useDebugValue, useState } from "react";
 function CandidateSuccess() {
   let url = "";
   return (
-    <div className="flex flex-col">
-      <div className="bg-green-succ mx-10 h-max mt-30 rounded flex flex-row-nowrap">
-        <div className="icon">
+    <div className="flex flex-col items-center justify-center mx-10">
+      <div className="bg-green-succ h-max mt-30 rounded flex text-center w-full">
+        <div className="icon w-1/6">
           <img
             className="object-contain h-5 w-5 m-2 "
             src="https://img.icons8.com/color/48/000000/ok--v1.png"
@@ -13,19 +13,19 @@ function CandidateSuccess() {
           />
         </div>
 
-        <div className=" m-1 px-10 grow h-2">
+        <div className=" px-10 h-2 w-4/6 mt-1">
           Candidate Registration Success
         </div>
+        
       </div>
 
-      <div className="text-center m-10">
+      <div className="text-center my-8 ">
         You can login with Application No: as Username, Password and Applied
         District
       </div>
-
-      {/* <div className="text-center m-10"> */}
-        <a className="text-center m-10" href={url}>Candidate Login</a>
-      {/* </div> */}
+      <button className="btn w-54 mb-5">Candidate Login</button>
+      
+      
     
     </div>
   );
