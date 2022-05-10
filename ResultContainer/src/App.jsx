6679196872ce0685c4f939d274f4b3934f1f5039
rj777pain <rejathtm@gmail>
2022-05-10 15:12:01 +0530

    added result container

---
+++ ResultContainer/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import ResultScreen from './ResultScreen'
+
+function App() {
+  
+
+  return (
+    <div className='w-full flex justify-center bg-slate-200 h-screen'>
+    <div className='w-4/5 center'><ResultScreen/></div>
+    </div>
+  )
+}
+
+export default App
---
+++ ResultContainer/src/ResultScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ResultScreen() {
+  return (
+    <div className='bg-white h-max mt-20 rounded-lg'>
+        <div className='text-center font-medium text-xl uppercase pt-10'>Allotment Result</div><br/>
+    </div>
+        
+  )
+}
+
+export default ResultScreen
