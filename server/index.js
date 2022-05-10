c38ad21abf685120d55582db914e9fdfa81275ba
pkNandu <nandupkputhiyakunnon@gmail.com>
2022-05-10 16:03:30 +0530

    changed index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   user: "root",
   host: "localhost",
-  password: "password",
+  password: "362394",
   database: "admission",
 });
 
@@ -23,7 +23,7 @@ app.post("/create", (req, res) => {
   const modeOfApplication = req.body.modeOfApplication;
 
   db.query(
-    "INSERT INTO employees (sslcScheme, registerNumber, monthPass, yearPass, dateOfBirth, nummber, modeOfAPplication) VALUES (?,?,?,?,?)",
+    "INSERT INTO employees (sslcScheme, registerNumber, monthPass, yearPass, dateOfBirth, nummber, modeOfAPplication) VALUES (?,?,?,?,?,?,?)",
     [sslcScheme, registerNumber, monthPass, yearPass, dateOfBirth, nummber, modeOfApplication ],
     (err, result) => {
       if (err) {
@@ -33,43 +33,6 @@ app.post("/create", (req, res) => {
       }
     }
   );
-});
-
-app.get("/employees", (req, res) => {
-  db.query("SELECT * FROM student", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-app.put("/update", (req, res) => {
-  const id = req.body.id;
-  const nummber = req.body.nummber;
-  db.query(
-    "UPDATE student SET nummber = ? WHERE id = ?",
-    [nummber, id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
-});
-
-app.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  db.query("DELETE FROM student WHERE id = ?", id, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
 });
 
 app.listen(3001, () => {
